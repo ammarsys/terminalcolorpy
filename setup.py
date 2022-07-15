b376d4e6a5a1432733b37ab0d409110ae1a8651d
Ammar <amarftw1@gmail.com>
2022-07-15 02:23:58 +0200

    new version things

--- setup.py
+++ setup.py
@@ -7,26 +7,26 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="terminalcolorpy",
-    version="0.1.2",
-    author="ammarsys",
+    version="2.0.0",
+    author="novusys",
     author_email="amarftw1@gmail.com",
     description="Simple package to print colored messages using ASCI to the terminal.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ammarsys/terminalcolorpy/",
+    url="https://github.com/novusys/terminalcolorpy/",
     project_urls={
-        "Bug Tracker": "https://github.com/ammarsys/terminalcolorpy/issues",
+        "Bug Tracker": "https://github.com/novusys/terminalcolorpy/issues",
     },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    packages=["pyaww"],
+    packages=["terminalcolorpy"],
     install_requires=[],
-    python_requires=">=3.6",
+    python_requires=">=3.10",
     license="MIT",
 )
